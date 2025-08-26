1b57e718838a268b320fa472ac5d8ef1e555fdb4
chase <chase>
2025-08-26 12:54:05 -0400

    python3 test

--- banner.py
+++ banner.py
@@ -1,29 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Xerosploit banners
-
-
-#---------------------------------------------------------------------------#
-# This file is part of Xerosploit.                                          #
-# Xerosploit is free software: you can redistribute it and/or modify        #
-# it under the terms of the GNU General Public License as published by      #
-# the Free Software Foundation, either version 3 of the License, or         #
-# (at your option) any later version.                                       #
-#                                                                           #
-# Xerosploit is distributed in the hope that it will be useful,             #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-# GNU General Public License for more details.                              #
-#                                                                           #
-# You should have received a copy of the GNU General Public License         #
-# along with Xerosploit.  If not, see <http://www.gnu.org/licenses/>.       #
-#                                                                           #
-#---------------------------------------------------------------------------#
-#                                                                           #
-#        Copyright © 2019 Neodrix (www.neodrix.com)                         #
-#                                                                           #
-#---------------------------------------------------------------------------#
-
 
 import random
 
@@ -52,7 +29,7 @@ header2 = """
 
 header3 = """
 
-        ▄  ▄███▄   █▄▄▄▄ ████▄    ▄▄▄▄▄   █ ▄▄  █     ████▄ ▄█    ▄▄▄▄▀ 
+  ▄  ▄███▄   █▄▄▄▄ ████▄    ▄▄▄▄▄   █ ▄▄  █     ████▄ ▄█    ▄▄▄▄▀ 
     ▀▄   █ █▀   ▀  █  ▄▀ █   █   █     ▀▄ █   █ █     █   █ ██ ▀▀▀ █    
       █ ▀  ██▄▄    █▀▀▌  █   █ ▄  ▀▀▀▀▄   █▀▀▀  █     █   █ ██     █    
      ▄ █   █▄   ▄▀ █  █  ▀████  ▀▄▄▄▄▀    █     ███▄  ▀████ ▐█    █     
@@ -66,7 +43,7 @@ __  |/ /_____ ______________ \033[1;36m__  ___/________ ___  /______ ___(_)__  /
 __    / _  _ \__  ___/_  __ \\\033[1;36m_____ \ ___  __ \__  / _  __ \__  / _  __/\033[1;m
 _    |  /  __/_  /    / /_/ /\033[1;36m____/ / __  /_/ /_  /  / /_/ /_  /  / /_  \033[1;m
 /_/|_|  \___/ /_/     \____/ \033[1;36m/____/  _  .___/ /_/   \____/ /_/   \__/  \033[1;m
-                             \033[1;36m        /_/                               \033[1;m     
+           \033[1;36m        /_/                               \033[1;m     
 """
 
 def xe_header():
